d3b6803039ed722f536beed6ba267d273be52869
andresguisado <andresguisado@ag-contino.local>
2017-11-03 18:26:28 +0100

    Blog update at 2017-11-03 17:26:27

--- update_blog.sh
+++ update_blog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 buster generate
-find static -name *.html -type f -exec sed -i '' 's#http://localhost:2368#https://www.andresguisado.com#g' {} \;
+#find static -name *.html -type f -exec sed -i '' 's#http://localhost:2368#https://www.andresguisado.com#g' {} \;
 buster deploy
 #git add -A
 #git commit -m "Update on the website at $(date)"
